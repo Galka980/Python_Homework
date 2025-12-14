07afcda36bf07a0bfa50eb21ca489b6584731a38
Galka980 <g24742367@gmail.com>
2025-12-14 16:18:52 +0700

    Lesson1

--- Lesson1/lesson_1_task_3.py
+++ Lesson1/lesson_1_task_3.py
@@ -1,4 +1,2 @@
-first_name = input("Введите ваше имя: ")
-last_name = input("Введите вашу фамилию: ")
-print("Имя:", first_name)
-print("Фамилия:", last_name)
+first_name = input("Вас зовут: last_name first_name")
+print(first_name)
