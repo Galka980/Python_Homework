db8cf65f362588c635349b9d38853dedd7b18e9c
Galka980 <g24742367@gmail.com>
2025-12-15 00:38:16 +0700

    Lesson1

--- Lesson1/practical work/practical work.py
+++ Lesson1/practical work/practical work.py
@@ -15,8 +15,7 @@ print_python()
 
 
 def print_letter(let):
-    print(let, end='')
-
+    print(let, end = '')
 
 print_letter('C')
 print_letter('т')
@@ -25,7 +24,6 @@ print_letter('д')
 print_letter('е')
 print_letter('н')
 print_letter('т')
-
 
 pet_name = input("Как зовут вашего питомца? ")
 print("Ваш любимчик - " + pet_name)
